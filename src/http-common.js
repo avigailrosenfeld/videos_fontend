ae6edc176f510dc0a223e428343b4f3052c75497
Avigail Rosenfeld <avigil1213@gmail.com>
2022-01-12 13:12:04 +0200

    remove fix cors

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://127.0.0.1:8000",
   headers: {
-    "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "*"
+    "Content-type": "application/json"
   }
 });
