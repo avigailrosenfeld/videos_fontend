6d9ff0a37f88beeb8d516369bfa2db64dc538e8e
Avigail Rosenfeld <avigil1213@gmail.com>
2022-02-16 11:05:00 +0000

    save fields of create

--- src/components/videos/CreateVideoModal.vue
+++ src/components/videos/CreateVideoModal.vue
@@ -7,34 +7,160 @@
       @hidden="resetCreateModal"
       @ok="initiateCreate"
       >
-      <form ref="form" @submit.stop.prevent="initiateCreate" enctype="multipart/form-data"> 
+      <form ref="form" @submit.stop.prevent="initiateCreate" enctype="multipart/form-data">
+      <b-form-row> 
+        <b-col>
+          <b-form-group
+            label="File Name:"
+            label-for="filename-input"
+            invalid-feedback="FileName is required"
+            :state="filenameState"
+          ></b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-input      
+            id="filename-input"
+            v-model="filename"
+            placeholder="Enter your file name"
+            :state="filenameState"
+            required
+          ></b-form-input>
+        </b-col>
+        </b-form-row>
+
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Private:"
+              label-for="private-checkbox"
+              invalid-feedback="Private is required"
+              :state="privateState"
+              required
+            ></b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-checkbox
+              id="private-checkbox"
+              v-model="privatevideo"
+              :state="privateState"
+            ></b-form-checkbox>
+          </b-col>
+        </b-form-row>
+
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Allow anonymous embed:"
+              label-for="anonymousembed-checkbox"
+              invalid-feedback="Anonymous embed is required"
+              :state="anonymousembedState"
+            ></b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-checkbox
+              id="anonymousembed-checkbox"
+              v-model="anonymousembed"
+              :state="anonymousembedState"
+            ></b-form-checkbox>
+          </b-col>
+        </b-form-row>
+        
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Tags:"
+              label-for="tags-select"
+              invalid-feedback="Tags is required"
+              :state="tagsState"
+            ></b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-select
+              id="tags-select"
+              v-model="tags"
+              :state="tagsState"
+            >
+              <template #first>
+                <b-form-select-option :value="null" disabled>-- Please select tags --</b-form-select-option>
+              </template>
+              <!-- These options will appear after the ones from 'options' prop -->
+              <b-form-select-option value="A">Tag A</b-form-select-option>
+              <b-form-select-option value="B">Tag B</b-form-select-option>
+            </b-form-select>
+          </b-col>
+        </b-form-row>
+
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Owner:"
+              label-for="owner-select"
+              invalid-feedback="Owner is required"
+              :state="ownerState"
+            ></b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-select
+              id="owner-select"
+              v-model="owner"
+              :state="ownerState"
+            >
+              <template #first>
+                <b-form-select-option :value="null" disabled>-- Please select Owner --</b-form-select-option>
+              </template>
+              <!-- These options will appear after the ones from 'options' prop -->
+              <b-form-select-option value="A">Owner A</b-form-select-option>
+              <b-form-select-option value="B">Owner B</b-form-select-option>
+            </b-form-select>
+          </b-col>
+        </b-form-row>
+
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Deletion Date:"
+              label-for="deletiondate-input"
+              invalid-feedback="Deletion Date is required"
+              :state="deletionDateState"
+              v-slot="{ ariaDescribedby }"
+            >
+              <b-form-checkbox-group
+                v-model="deletiondatecheckbox"
+                :aria-describedby="ariaDescribedby"
+                aria-controls="ex-disabled-readonly"
+                switches
+              >
+                <b-form-checkbox
+                id="deletiondate-checkbox"
+                value=[1]
+                ></b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group> 
+          </b-col>
+          <b-col>
+            <b-form-datepicker 
+              id="datepicker"
+              :disabled="disabled"
+            ></b-form-datepicker>
+          </b-col>
+        </b-form-row>
+
         <b-form-group
-          label="FileName"
-          label-for="filename-input"
-          invalid-feedback="FileName is required"
-          :state="filenameState"
-        >
-        <b-form-input
-          id="filename-input"
-          v-model="filename"
-          placeholder="Enter your file name"
-          :state="filenameState"
-          required
-        ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="VideoFile"
+          label="File:"
           label-for="video-file-input"
           invalid-feedback="Video File is required"
-          :state="Boolean(videofile)"
+          :state="videoFileState"
         >
           <b-form-file
             id="video-file-input"
             v-model="videofile"
-            accept=".mp4"
+            accept="*.aac|*.aif|*.aifc|*.aiff|*.au|*.flac|*.m3u|*.m4a|*.mp3|*.oga|*.ogg|*.ra|*.ram|*.rm|*.wav|*.wma
+                    *.3gp|*.asf|*.avi|*.dif|*.dv|*.f4v|*.flv|*.fmp4|*.m4v|*.mov|*.movie2|*.mp4|*.mpe|*.mpeg|*.mpg|*.ogv 
+                    *.qt|*.rmvb|*.rv|*.swf|*.swfl|*.ts|*.webm|*.wmv"
             placeholder="Choose a file or drop it here..."
-            :state="Boolean(videofile)"
+            :state="videoFileState"
             required
+            multiple
           ></b-form-file>
         </b-form-group>
       </form>
@@ -49,28 +175,36 @@ export default {
   name: "create-video-modal",
   data() {
     return {
-      filename:'123',
+      filename:'',
       filenameState: null,
-      videofile:'123',
-      videoFileState: null,
-      length:'123',
-      lengthState: null,
-      width:'123',
-      widthState: null,
-      height:'123',
-      heightState: null,
-      size:'123',
-      sizeState: null,
+      privatevideo: [],
+      privateState: null,
+      anonymousembed: [],
+      anonymousembedState: null,
+      tags: '',
+      tagsState: null,
+      owner: '',
+      ownerState: null,
+      deletionDate: '',
+      deletionDateState: null,
+      deletiondatecheckbox: [0],
+      videofile:[],
+      videoFileState: null
      };
+  },
+  computed: {
+      disabled() {
+        return this.deletiondatecheckbox === [0]
+      }
   },
   methods: {
     resetCreateModal() {
         this.filename = ''
         this.filenameState = null
-        this.videofile = ''
+        this.videofile = []
         this.videoFileState = null
-        this.length = ''
-        this.lengthState = null
+        this.privatevideo = ''
+        this.privateState = null
         this.width = ''
         this.widthState = null
         this.height = ''
@@ -82,7 +216,7 @@ export default {
         const valid = this.$refs.form.checkValidity()
         this.filenameState = valid
         this.videoFileState = valid
-        this.lengthState = valid
+        this.privateState = valid
         this.widthState = valid
         this.heightState =  valid
         this.sizeState = valid
@@ -108,7 +242,7 @@ export default {
         formData.append('filename' ,this.filename);
 
 
-       axios.post(`http://172.17.0.2:8000/videos`, formData,   {headers :  {
+       axios.post(`http://172.17.0.3:8000/videos`, formData,   {headers :  {
       "Authorization": `Bearer ${localStorage.getItem('token')}`,
       "Content-Type": `multipart/form-data; boundary=${formData._boundary}`
       
@@ -136,3 +270,8 @@ export default {
   }
 };
 </script>
+<style >
+.form-row{
+  margin-bottom: 10px;
+}
+</style>
